051f4870b33b412cec4c0bcdb42ade36d40cd5cb
Tuğçe Eraslan <tugceeraslan@emekkitap.com>
2022-02-28 15:13:08 +0300

    Add project files.

---
+++ MovieApp/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieApp.Controllers
+{
+    public class HomeController : Controller  /* Kullanıcı sayfaları için HomeController kullanılır */
+    {
+        public IActionResult Index()  // Kullanıcı anasayfaya geldiğinde sadece onaylı movie bilgilerini görebilir
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        } 
+    }
+}
---
+++ MovieApp/Controllers/MovieController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieApp.Controllers
+{
+    public class MovieController : Controller      /* MovieController içerisinde Admin'in yapması gereken işleri yapalım. */
+    {
+        public IActionResult Index()       // Kullanıcıya ilk gösterilen alan( Bütün Movie bilgilerini gösterir )
+        {
+            return View();
+        }
+
+        public IActionResult Create()      // Kayıt ekleme işlemi yapalım
+        {
+            return View();
+        }
+
+        public IActionResult Details()    // Detay bilgileri için
+        {
+            return View();
+        }
+
+        public IActionResult List()       // Listeleme işlemi yapalım
+        {
+            return View();
+        }
+    }
+}
