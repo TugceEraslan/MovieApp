7a3df5fcbef44b65e494c846f3f7fb73545c8009
Tuğçe Eraslan <tugceeraslan@emekkitap.com>
2022-05-30 13:06:14 +0300

    + Movie Details sayfası için detayda açıklama olacak şekilde eklemeler yapıldı. ShortDescription üzerinden ilerlendi.

--- MovieApp/Controllers/HomeController.cs
+++ MovieApp/Controllers/HomeController.cs
@@ -15,6 +15,12 @@ namespace MovieApp.Controllers
                                               // Ama index.cshtml inden de bu bilgileri almak için index.cshtml üzerinde de model oluşturmam gerekir
         }
 
+        public IActionResult Details(int id)   // _movies.cshtml de ki  asp-route-id="@movie.Id" için home controller daki details action undan beklenen id olacak
+        {
+            return View(Repository.GetById(id));   // GetById(id) benden Details avtion undan gönderilen id parametresi bekliyor
+                                                   // ve bende bu id gönderince Repository.GetById(id) ile bana tek bir movie gönderiyor
+        }
+
         public IActionResult Contact()
         {
             return View();
--- MovieApp/Models/Repository.cs
+++ MovieApp/Models/Repository.cs
@@ -16,13 +16,46 @@ namespace MovieApp.Models
             _movies = new List<Movie>()    // Movie içerisine örnek bir liste ekleyeceğim
             {
                 // Liste içerisine bir film listesi ekleyelim
-                new Movie() { Id=1, Name="Shazam", Description="Shazam", ImageUrl="1.jpg"},
-                new Movie() { Id=2, Name="Amazing Grace", Description="Amazing Grace", ImageUrl="2.jpg"},
-                new Movie() { Id=3, Name="High Life", Description="High Life", ImageUrl="3.jpg"},
-                new Movie() { Id=4, Name="Billboard", Description="Billboard", ImageUrl="4.jpg"},
-                new Movie() { Id=5, Name="Storm Boy", Description="Storm Boy", ImageUrl="5.jpg"}
-
-
+                new Movie() 
+                { 
+                    Id=1, 
+                    Name="Shazam", 
+                    ShortDescription="Shazam",
+                    Description="<p>A beautiful and contemporary retelling of Colin Thiele's classic Australian tale. 'Storm Boy' has grown up to be Michael Kingley, a successful retired businessman and grandfather.</p> <p>When Kingley starts to see images from his past that he can't explain, he is forced to remember his long-forgotten childhood, growing up on an isolated coastline with his father. He recounts to his grand-daughter the story of how, as a boy, he rescued and raised an extraordinary orphaned pelican, Mr Percival.</p><p> Their remarkable adventures and very special bond has a profound effect on all their lives. Based on the beloved book, Storm Boy is a timeless story of an unusual and unconditional friendship.</p>", 
+                    ImageUrl="1.jpg"
+                },
+                new Movie() 
+                { 
+                    Id=2, 
+                    Name="Amazing Grace",
+                    ShortDescription="Amazing Grace",
+                    Description="<p>A beautiful and contemporary retelling of Colin Thiele's classic Australian tale. 'Storm Boy' has grown up to be Michael Kingley, a successful retired businessman and grandfather.</p> <p>When Kingley starts to see images from his past that he can't explain, he is forced to remember his long-forgotten childhood, growing up on an isolated coastline with his father. He recounts to his grand-daughter the story of how, as a boy, he rescued and raised an extraordinary orphaned pelican, Mr Percival.</p><p> Their remarkable adventures and very special bond has a profound effect on all their lives. Based on the beloved book, Storm Boy is a timeless story of an unusual and unconditional friendship.</p>", 
+                    ImageUrl="2.jpg"
+                },
+                new Movie() 
+                { 
+                    Id=3, 
+                    Name="High Life",
+                    ShortDescription="High Life",
+                    Description="<p>A beautiful and contemporary retelling of Colin Thiele's classic Australian tale. 'Storm Boy' has grown up to be Michael Kingley, a successful retired businessman and grandfather.</p> <p>When Kingley starts to see images from his past that he can't explain, he is forced to remember his long-forgotten childhood, growing up on an isolated coastline with his father. He recounts to his grand-daughter the story of how, as a boy, he rescued and raised an extraordinary orphaned pelican, Mr Percival.</p><p> Their remarkable adventures and very special bond has a profound effect on all their lives. Based on the beloved book, Storm Boy is a timeless story of an unusual and unconditional friendship.</p>", 
+                    ImageUrl="3.jpg"
+                },
+                new Movie() 
+                { 
+                    Id=4, 
+                    Name="Billboard",
+                    ShortDescription="Billboard",
+                    Description="<p>A beautiful and contemporary retelling of Colin Thiele's classic Australian tale. 'Storm Boy' has grown up to be Michael Kingley, a successful retired businessman and grandfather.</p> <p>When Kingley starts to see images from his past that he can't explain, he is forced to remember his long-forgotten childhood, growing up on an isolated coastline with his father. He recounts to his grand-daughter the story of how, as a boy, he rescued and raised an extraordinary orphaned pelican, Mr Percival.</p><p> Their remarkable adventures and very special bond has a profound effect on all their lives. Based on the beloved book, Storm Boy is a timeless story of an unusual and unconditional friendship.</p>", 
+                    ImageUrl="4.jpg"
+                },
+                new Movie() 
+                {
+                    Id=5, 
+                    Name="Storm Boy",
+                    ShortDescription="Storm Boy",
+                    Description="<p>A beautiful and contemporary retelling of Colin Thiele's classic Australian tale. 'Storm Boy' has grown up to be Michael Kingley, a successful retired businessman and grandfather.</p> <p>When Kingley starts to see images from his past that he can't explain, he is forced to remember his long-forgotten childhood, growing up on an isolated coastline with his father. He recounts to his grand-daughter the story of how, as a boy, he rescued and raised an extraordinary orphaned pelican, Mr Percival.</p><p> Their remarkable adventures and very special bond has a profound effect on all their lives. Based on the beloved book, Storm Boy is a timeless story of an unusual and unconditional friendship.</p>",
+                    ImageUrl="5.jpg"
+                }
             };
         }
         //Repository.movies dediğimde bana bilgi getirmesi gerek
